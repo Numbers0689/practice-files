575fb92131eed786b41bb2c19e04a1bba833308f
Numbers0689 <firewatchsai@gmail.com>
2023-10-29 19:00:18 +0530

    Practice string

--- newc/factre.c
+++ newc/factre.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
-long int multiplyNumbers(int n);
+unsigned long long multiplyNumbers(int n);
 int main() {
     int n;
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-    printf("Factorial of %d = %ld\n", n, multiplyNumbers(n));
+    printf("Factorial of %d = %llu\n", n, multiplyNumbers(n));
     return 0;
 }
 
-long int multiplyNumbers(int n) {
+unsigned long long multiplyNumbers(int n) {
     if (n>=1)
         return n*multiplyNumbers(n-1);
     else
---
+++ newc/str.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char name[10];
+    printf("Enter a string to print: ");
+    scanf("%s",name);
+    printf("your string is %s\n", name);
+}
