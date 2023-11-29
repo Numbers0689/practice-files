58905565c69b68dbfe44e8ecd064a5da837efefe
Numbers0689 <firewatchsai@gmail.com>
2023-11-29 22:59:39 +0530

    Make assert preprocessor function to debug

---
+++ newc/assert.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+
+#define DEBUG
+
+#ifndef DEBUG
+#define ASSERT(n)
+#else 
+#define ASSERT(n) \
+if(!(n)) { \
+printf("%s - FAILED ", #n); \
+printf("On %s ", __DATE__); \
+printf("At %s ", __TIME__); \
+printf("In File %s ", __FILE__); \
+printf("At Line %d\n", __LINE__); \
+exit(1); }
+#endif
