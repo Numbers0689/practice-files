d448b8e5dfe7e7ae001076c8378cd1e45672268b
Numbers0689 <firewatchsai@gmail.com>
2023-12-10 21:44:57 +0530

    Create strlen fn

--- mylib/test.c
+++ mylib/test.c
@@ -1,1 +1,16 @@
 // gonna be adding everthing to make my library here in the folder, this ll be the where im gon test them out
+
+ #include <stdio.h>
+
+ int main(int argc, char* argv[]) 
+ {
+    char string[20] = "lol really?";
+    int count = 0;
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        count++;   
+    }
+
+    printf("length: %d", count);
+    return 0;
+ }
