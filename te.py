b710269ed6b5349776529b7818851eae95ae53e8
Numbers0689 <firewatchsai@gmail.com>
2024-06-04 17:34:22 +0530

    Create tkinter func

--- te.py
+++ te.py
@@ -1,9 +1,27 @@
-py=[]
+import tkinter as tk
 
-for i in range(4):
-    py.append([])
-    for j in range(i+1):
-        if j==0 or j==i:
-            py[i].append(1)
-        else:
-            py[i].append(sum(py[i-1][j-1:j+1]))
+r = tk.Tk()
+r.title("blah blah")
+r.geometry("800x450")
+
+
+def display():
+    total = 0
+    mapE()
+
+
+def mapE(E, ro, c, marks, credit):
+    if E.get() == 'A':
+        tk.Label(r, text=str(marks)).grid(row=ro, column=c)
+        total += marks
+    if E.get() == 'B':
+        tk.Label(r, text=str(marks - (credit))).grid(row=ro, column=c)
+        total += marks - (credit)
+    if E.get() == 'C':
+        tk.Label(r, text=str(marks - (credit * 2))).grid(row=ro, column=c)
+        total += marks - (credit * 2)
+    if E.get() == 'D':
+        tk.Label(r, text=str(marks - (credit * 3))).grid(row=ro, column=c)
+        total += marks - (credit * 3)
+
+r.mainloop()
