2f74355867d2e36fbc25ce844c67762c9498099d
Numbers0689 <firewatchsai@gmail.com>
2024-09-01 19:23:00 +0530

    Fs module

--- odin/backend/wsapp.js
+++ odin/backend/wsapp.js
@@ -1,6 +1,10 @@
 const http = require("http");
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('dont connnet to my server, not on this port!');
+	fs.readFile('num.html', function (err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
 }).listen(8080);
