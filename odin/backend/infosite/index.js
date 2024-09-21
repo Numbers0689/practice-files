e151a264808ccc74e3ff24459b7ba44e47a533b1
Numbers0689 <firewatchsai@gmail.com>
2024-09-21 23:36:39 +0530

    Rewrite app in express

--- odin/backend/infosite/index.js
+++ odin/backend/infosite/index.js
@@ -1,29 +1,26 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
-const pg404 = fs.readFileSync("404.html", 'utf-8', (err, data) => {
-    if (err) throw err;
-    return data;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    let filename = "";
-    if (q.pathname === '/') {
-        filename = "./index.html";
-    } else {
-        filename = "." + q.pathname;
-    }
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+});
 
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write(pg404);
-            return res.end();
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'about.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'contact.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'404.html'));
+});
+
+app.listen(3000);
