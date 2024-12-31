df39e7a04ef90c40ee65c0828f46b1addcb1e877
Numbers0689 <firewatchsai@gmail.com>
2024-12-31 16:14:04 +0530

    Complete VMtranslator P07

--- n2t/07/parser/Command.java
+++ n2t/07/parser/Command.java
@@ -4,10 +4,10 @@ public enum Command {
     C_ARITHMETIC,
     C_PUSH,
     C_POP,
-    C_LABLE,
-    C_FUNCTION,
-    C_GOTO,
-    C_CALL,
-    C_RETURN,
+    // C_LABLE,
+    // C_FUNCTION,
+    // C_GOTO,
+    // C_CALL,
+    // C_RETURN,
     C_ERROR
 }
