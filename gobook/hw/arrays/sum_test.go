8bf3ab4ba8e6c03b2182c9ca93da456753e87cc7
Numbers0689 <firewatchsai@gmail.com>
2025-04-16 22:09:37 +0530

    more go stuff - slices

--- gobook/hw/arrays/sum_test.go
+++ gobook/hw/arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -10,5 +13,23 @@ func TestSum(t *testing.T) {
 
 	if got != expected {
 		t.Errorf("expected '%d' but got '%d', given %v", expected, got, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 3, 3})
+	expected := []int{3, 9}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{3, 3, 3})
+	expected := []int{2, 6}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
 	}
 }
