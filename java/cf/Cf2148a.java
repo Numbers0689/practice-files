35c74670c9392d5b5c820c01a8de33d55b7644ba
Numbers0689 <firewatchsai@gmail.com>
2025-09-14 22:30:06 +0530

    CF div 4

---
+++ java/cf/Cf2148a.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2148a {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt(), n = sc.nextInt();
+            if (n%2 == 0) System.out.println(0);
+            else System.out.println(x);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
