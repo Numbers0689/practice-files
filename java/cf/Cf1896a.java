01630b7bdc979a5872fd4813fa14498e66965424
Numbers0689 <firewatchsai@gmail.com>
2025-09-18 22:50:07 +0530

    cf pracs

---
+++ java/cf/Cf1896a.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf1896a {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            int m = a[0];
+            for (int i = 0; i < n; i++) {
+                if (m > a[i]) {
+                    System.out.println("NO");
+                    m = a[i];
+                    break;
+                }
+            }
+
+            if (m == a[0]) System.out.println("YES");
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
