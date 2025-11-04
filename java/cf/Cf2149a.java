fc67074319917563b37eeacbcab2fbce7d1de4c2
Numbers0689 <firewatchsai@gmail.com>
2025-11-04 22:47:16 +0530

    CF 2149

---
+++ java/cf/Cf2149a.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2149a {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int c = 0, o = 0;
+            for (int i = 0; i < n; i++) {
+                int a = sc.nextInt();
+                if (a == 0) c++;
+                else if (a == -1) o++;
+            }
+            System.out.println(c + (o%2)*2);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
