f9ec1f15a2dfe1101927ba18efb0d031650c48f2
Numbers0689 <firewatchsai@gmail.com>
2025-11-07 23:03:03 +0530

    CF 800s

--- java/cf/Cf2155a.java
+++ java/cf/Cf2155a.java
@@ -1,25 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-public class Cf2149c {
+public class Cf2155a {
     public static void main(String[] args) {
         FastScanner sc = new FastScanner();
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
-            int k = sc.nextInt();
-            int[] a = new int[n];
-            int[] e = new int[k];
-            int c = 0,x = 0;
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-                if (a[i] == k) x++;
-                if (a[i] < k) e[a[i]]++;
+            int w = n, l = 0, c = 0;
+            while (w > 1 || l > 1) {
+                if (l > 1) {
+                    c += l/2;
+                    l = l/2 + l%2;
+                }
+                if (w > 1) {
+                    c += w/2;
+                    l += w/2;
+                    w = w/2 + w%2;
+                }
             }
-            for (int i = 0; i < k; i++) {
-                if (e[i] == 0) c++;
-            }
-            System.out.println(Math.max(x, c));
+            System.out.println(c+1);
         }
     }
 }
