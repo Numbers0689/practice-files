36c942960a2e313ce926b22bdb652690e622280a
Numbers0689 <firewatchsai@gmail.com>
2025-11-16 22:32:06 +0530

    CF div 2 16/11

---
+++ java/cf/Cf2166a.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2166a {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            int c = 0;
+            char e = s.charAt(n-1);
+            for (int i = n-2; i >= 0; i--) {
+                if (s.charAt(i) != e) c++;
+            }
+            System.out.println(c);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
