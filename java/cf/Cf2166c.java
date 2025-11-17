be3978bcea9f48497a0ad16da0c6aa343e226dc0
Numbers0689 <firewatchsai@gmail.com>
2025-11-17 18:57:40 +0530

    CF pracs

---
+++ java/cf/Cf2166c.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2166c {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            long c, sum = 0, maxi = 0;
+            for (int i = 0; i < n; i++) {
+                c = Math.max(a[i], a[(i+1)%n]);
+                maxi = Math.max(c, maxi);
+                sum += c;
+            }
+            System.out.println(sum-maxi);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
