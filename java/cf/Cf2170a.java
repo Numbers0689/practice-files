76755b4755b710bba727e495d83ec720c56e1c4a
Numbers0689 <firewatchsai@gmail.com>
2025-11-29 23:46:30 +0530

    CF pracs

--- java/cf/Cf2170a.java
+++ java/cf/Cf2170a.java
@@ -1,12 +1,21 @@
 import java.util.*;
 import java.io.*;
 
-public class Template {
+public class Cf2170a {
     public static void main(String[] args) {
         FastScanner sc = new FastScanner();
         int t = sc.nextInt();
         while (t-- > 0) {
-            
+            int n = sc.nextInt();
+            if (n == 1) System.out.println(1);
+            else if (n == 2) System.out.println(9);
+            else if (n == 3) System.out.println(29);
+            else if (n == 4) System.out.println(56);
+            else {
+                int x = (n-1)*n - 1;
+                int s = 5*x;
+                System.out.println(s);
+            }
         }
     }
 }
