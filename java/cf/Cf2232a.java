179db96a15a88dcc06d150852ff957daf66b0bce
Numbers0689 <firewatchsai@gmail.com>
2026-06-02 23:29:13 +0530

    CF pracs

---
+++ java/cf/Cf2232a.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2232a {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            Arrays.sort(a);
+            int count = 0;
+            for (int i = 0;i < n; i++) if (a[i] != a[n - 1 - i]) count++;
+            System.out.println(count/2);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
