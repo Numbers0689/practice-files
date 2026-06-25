396bdf2237cb997b0b2b5da79021b06fd9fb7780
Numbers0689 <firewatchsai@gmail.com>
2026-06-25 23:10:24 +0530

    qp dump

--- java/cf/Com.java
+++ java/cf/Com.java
@@ -11,6 +11,104 @@ public class Com {
     }
 }
 
+// interface List {
+//     public void add(int index, Integer e);
+//     public void addLast(Integer e);
+//     public void addFirst(Integer e);
+//     public Integer get(int index);
+//     public void remove(int index);
+// }
+
+class LinkedList {
+
+    // @Override
+    public void add(int index, Integer e) {
+
+    }
+
+    // @Override
+    public void addLast(Integer e) {
+
+    }
+
+    // @Override
+    public void addFirst(Integer e) {
+
+    }
+
+    // @Override
+    public Integer get(int index) {
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    // @Override
+    public void remove(int index) {
+
+    }
+}
+
+class Stack {
+    int cap;
+    List<Integer> stack;
+
+    Stack() {
+        cap = 10;
+        stack = new ArrayList<>();
+    }
+
+    public void push(Integer e) {
+        stack.add(e);
+    }
+
+    public Integer pop() {
+        return stack.removeLast();
+    }
+
+    public Integer peek() {
+        return stack.getLast();
+    }
+}
+
+class ArrayQueue {
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int capacity;
+    private int size;
+
+    public ArrayQueue(int capacity) {
+        this.capacity = capacity;
+        this.queue = new int[capacity];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public void enqueue(int item) {
+
+    }
+
+    public int dequeue() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean isFull() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int getSize() {
+
+    }
+}
+
 class FastScanner {
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st=new StringTokenizer("");
