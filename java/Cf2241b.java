6f9bc9a9549e7ec600ac4798224820615cae313d
Numbers0689 <firewatchsai@gmail.com>
2026-07-09 23:44:00 +0530

    more cf

---
+++ java/Cf2241b.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2241b {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int d = 0;
+
+            while (x > 0) {
+                d++;
+                x /= 10;
+            }
+            System.out.println((int)Math.pow(10, d)+1);
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
