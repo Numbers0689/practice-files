efa1a013cd4349bfa84f87151b83aefc4fdfc4be
Numbers0689 <firewatchsai@gmail.com>
2026-08-06 14:42:32 +0530

    cf pracs

---
+++ java/cf/Cf2254c.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Cf2254c {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next(), s1 = sc.next();
+
+            int[] ca = new int[2], cb = new int[2];
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '1') ca[i%2]++;
+                if (s1.charAt(i) == '1') cb[i%2]++;
+            }
+
+            if (ca[0] == cb[0] && ca[1] == cb[1]) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
+
+class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+        while (!st.hasMoreTokens())
+            try { 
+                st=new StringTokenizer(br.readLine());				               
+            } catch (IOException e) {}
+        return st.nextToken();
+    }
+    
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+}
